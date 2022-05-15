c8dc4d464d9350233e204a03bd9e639adbe1a59f
marcusferl <marcusferl@web.de>
2022-05-15 21:28:30 +0200

    Projektdateien hinzufügen.

---
+++ MorseCoder/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MorseCoder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int start = 0;
+        private Dictionary<string, string> codeLegende = new Dictionary<string, string>();
+        private List<string> morseWord = new List<string>();
+        private Stopwatch sw = new Stopwatch();
+        public MainWindow()    
+        {
+            
+            MorseCode getCode = new MorseCode();
+            getCode.Code(codeLegende);
+            
+            InitializeComponent();
+        }
+        private void mousen_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+                sw.Start();
+        }
+        private void mousen_MouseUp(object sender, MouseButtonEventArgs e)
+        {   
+            sw.Stop();
+            if(sw.Elapsed.TotalMilliseconds > 500)
+            {
+                morseWord.Add("long");
+            }
+            else
+            {
+                morseWord.Add("short");
+            }
+            sw.Reset();
+            code_label.Content = String.Join(" ",morseWord);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach(KeyValuePair<string, string> pair in codeLegende)
+            {
+                if(pair.Value.Equals(String.Join(" ", morseWord)))
+                {
+                    decode_label.Content += pair.Key;
+                    morseWord.Clear();
+                }
+            }
+        }
+
+        private void Clear_Button_Click(object sender, RoutedEventArgs e)
+        {
+            code_label.Content = "";
+            decode_label.Content = "";
+            morseWord.Clear();
+        }
+    }
+    }
+    
---
+++ MorseCoder/MorseCode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MorseCoder
+{
+
+    internal class MorseCode
+    {
+        private Dictionary<string, string> code = new Dictionary<string, string>();
+
+        // Erstellung des KeyValuePair für den MorseCode
+        public Dictionary<string, string> Code(Dictionary<string, string> code)
+        {
+            this.code = code;
+            this.code.Add("A", "short long");
+            this.code.Add("B", "long short short short");
+            this.code.Add("C", "long short long short");
+            this.code.Add("D", "long short short");
+            this.code.Add("E", "short");
+            this.code.Add("F", "short short long short");
+            this.code.Add("G", "long long short");
+            this.code.Add("H", "short short short short");
+            this.code.Add("I", "short short");
+            this.code.Add("J", "short long long long");
+            this.code.Add("K", "long short long");
+            this.code.Add("L", "short long short short");
+            this.code.Add("M", "long long");
+            this.code.Add("N", "long short");
+            this.code.Add("O", "long long long");
+            this.code.Add("P", "shor long long short");
+            this.code.Add("Q", "long long short long");
+            this.code.Add("R", "short long short");
+            this.code.Add("S", "short short short");
+            this.code.Add("T", "long");
+            this.code.Add("U", "short short long");
+            this.code.Add("V", "short short short long");
+            this.code.Add("W", "short long long");
+            this.code.Add("X", "long short short long");
+            this.code.Add("Y", "long short long long");
+            this.code.Add("Z", "long long short short");
+            return this.code;
+        }
+    }
+}
