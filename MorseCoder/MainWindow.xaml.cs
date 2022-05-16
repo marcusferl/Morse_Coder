678c8cca59f2d1d4ff0226ea9bdffd8d3711a7de
marcusferl@weifer.de <marcus>
2022-05-16 11:56:58 +0200

    update

--- MorseCoder/MainWindow.xaml.cs
+++ MorseCoder/MainWindow.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace MorseCoder
 {
@@ -63,7 +54,6 @@ namespace MorseCoder
                 }
             }
         }
-
         private void Clear_Button_Click(object sender, RoutedEventArgs e)
         {
             code_label.Content = "";
@@ -71,5 +61,5 @@ namespace MorseCoder
             morseWord.Clear();
         }
     }
-    }
+  }
     
